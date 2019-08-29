68dfcf4f5c2f84c71db41a8495a21e4951bcf3ff
Ubuntu <ubuntu@ip-172-31-89-176.ec2.internal>
2019-08-29 11:49:25 +0000

    initial commit

---
+++ djangoblog/settings/local.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'i7smfe=04-v(=5osvk$437$y#mx_*^eha$530xn*c5u9yyj)6='
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
